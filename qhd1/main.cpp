de4f8ce24a601b48c835cc5abe3400ba0da03aa5
HieuK <hieutrankiemcong@gmail.com>
2024-07-30 08:31:43 +0700

    up

---
+++ qhd1/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    freopen("in.inp", "r", stdin);
+    freopen("out.out", "w", stdout);
+
+    cin >> n;
+
+    int h[n + 1];
+    int f[n + 1];
+    for (int i = 1; i <= n; i++)
+        cin >> h[i];
+
+    f[1] = 0;
+    f[2] = abs(h[2] - h[1]);
+    for (int i = 3; i <= n; i++)
+    {
+        f[i] = min(f[i - 1] + abs(h[i] - h[i - 1]), f[i - 2] + abs(h[i] - h[i - 2]));
+    }
+    cout << f[n];
+    return 0;
+}
---
+++ qhd2/main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    freopen("in.inp", "r", stdin);
+    freopen("out.out", "w", stdout);
+
+    cin >> n;
+
+        int t[n + 1];
+    int r[n + 1];
+    int f[n + 1];
+
+    for (int i = 1; i <= n; i++)
+        cin >> t[i];
+
+    for (int i = 1; i < n; i++)
+        cin >> r[i];
+
+    f[1] = t[1];
+    f[2] = min(t[1] + t[2], r[1]);
+
+    for (int i = 3; i <= n; i++)
+        f[i] = min(f[i - 1] + t[i], f[i - 2] + r[i - 1]);
+
+    cout << f[n];
+
+    return 0;
+}
